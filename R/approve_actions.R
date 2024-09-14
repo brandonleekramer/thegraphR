2f5ee02b043761da2ec39ef5b0fadcf56f7e5e8b
Brandon Kramer <brandonleekramer@gmail.com>
2024-09-14 16:34:05 -0400

    adding approve_actions()

--- R/approve_actions.R
+++ R/approve_actions.R
@@ -1,4 +1,14 @@
-
+#' Generate approve action command for Indexer allocations.
+#'
+#' @param actions Add numbers with first and last total separated by colon (:).
+#'
+#' @return A string to approve Indexer allocation actions.
+#' @export
+#'
+#' @examples
+#' #graph indexer actions queue allocate deployment_id grt_amount --network arbitrum-one
+#' approve_actions(1:4)
+#'
 approve_actions = function(actions){
   cat("graph indexer actions approve", c(actions),"--network arbitrum-one","\n")
   cat("graph indexer actions execute approved")
